1a87ab2c317cbb274c5509e1c60d0a742412d5ed
derwinlu <derwinlu+git@gmail.com>
2017-08-17 22:05:06 +0200

    Update build/push scripts

--- push.sh
+++ push.sh
@@ -7,17 +7,17 @@ DOCKER_USER="winlu"
 DOCKER_IMAGE="docker-git-server"
 
 
-build_image() {
+push_image() {
     IMAGE_VER=$1
     ALPINE_VER=$2
-    docker build --build-arg ALPINE_VER=$ALPINE_VER . -t $DOCKER_USER/$DOCKER_IMAGE:$IMAGE_VER-$ALPINE_VER
+    docker push $DOCKER_USER/$DOCKER_IMAGE:$IMAGE_VER-$ALPINE_VER
 }
 
 for t in `eval echo $ALPINE_VERSIONS`;
 do
     TAG=$DOCKER_USER/$DOCKER_IMAGE:$IMAGE_VERSION-$t
-    echo Building $TAG
-    build_image $IMAGE_VERSION $t
+    echo Pushing $TAG
+    push_image $IMAGE_VERSION $t
 done
 
-docker build --build-arg ALPINE_VER=3.6 . -t $DOCKER_USER/$DOCKER_IMAGE:latest
+docker push $DOCKER_USER/$DOCKER_IMAGE:latest
